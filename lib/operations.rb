59ec639185a32db908b0449a5f69db8a0d8e8204
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-05 21:36:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
-  if (speed < 40 == True) || (speed > 60 == True)
+  if (speed < 40 == true) || (speed > 60 == true)
     "True"
-  else
-    "False"
+   else
+   "False"
   end
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 && speed > 60 ? "False": "True"
+	speed < 40 == true && speed > 60 == true ? "True": "False"
 end
 	
 
